c4f10abc9ac3f3dd868f42b812b78d02a7750ba6
LorenzoCalc10 <lorenzo.calcagno@sancorsalud.com.ar>
2026-08-05 19:24:40 -0300

    Add files via upload
    Curso Data Analytics Práctica
    DDL y DML en SQL: administrando el inventario de BodegaTech

---
+++ SQLQuery 4-08.sql
@@ -0,0 +1,41 @@
+create database Prueba_BodegaTech;
+use Prueba_BodegaTech;
+DROP TABLE IF EXISTS inventario;
+
+CREATE TABLE  inventario (
+id_producto int not null primary key,
+nombre_producto VARCHAR(100),
+categoria VARCHAR(50),
+precio_unitario	DECIMAL(10,2),
+stock_actual INT,
+stock_minimo INT,
+fecha_ingreso DATE,
+
+);
+--Hasta acá se uso el lenguaje DDL
+
+INSERT INTO inventario (id_producto,nombre_producto,categoria,precio_unitario,stock_actual,stock_minimo,fecha_ingreso)
+VALUES
+(1, 'Laptop Pro 15', 'Computación', 1200.00, 15, 3, '2024-01-10'),
+(2, 'Mouse Inalámbrico', 'Accesorios', 28.00, 80, 10, '2024-01-10'),
+(3, 'Monitor 4K 27"', 'Computación', 450.00, 12, 2, '2024-01-15'),
+(4, 'Teclado Mecánico', 'Accesorios', 95.00, 40, 5, '2024-01-15'),
+(5, 'Laptop Basic 14', 'Computación', 650.00, 20, 3, '2024-02-01'),
+(6, 'Auriculares BT Pro', 'Audio', 120.00, 35, 5, '2024-02-01'),
+(7, 'Hub USB-C 7 puertos', 'Accesorios', 45.00, 60, 10, '2024-02-10'),
+(8, 'Webcam HD 1080p', 'Accesorios', 85.00, 25, 5, '2024-02-10'),
+(9, 'SSD Externo 1TB', 'Almacenamiento', 130.00, 18, 3, '2024-03-01'),
+(10, 'Parlante Bluetooth', 'Audio', 60.00, 45, 8, '2024-03-01');
+
+select*from inventario
+
+UPDATE inventario SET stock_actual = stock_actual - 6
+WHERE id_producto = 1;
+
+UPDATE inventario SET stock_actual = stock_actual - 12
+WHERE id_producto = 2;
+
+UPDATE inventario SET stock_actual = stock_actual - 5
+WHERE id_producto = 6;
+
+--hasta acá se uso el lnguaje DML
